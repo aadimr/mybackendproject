06aff0cc8ba6704ccd76a33e6975a9be72c0c347
aadimr <adityashaw9066@gmail.com>
2022-10-16 00:20:07 +0530

    assignment completed

---
+++ src/logger/logger.js
@@ -0,0 +1,6 @@
+let welcome = function () {
+    let name = "Aditya Shaw"
+    console.log(`Welcome to my application. I am ${name} and a part of FunctionUp lithium cohort.`)
+}
+
+module.exports.welcomefunction = welcome
---
+++ src/validator/formatter.js
@@ -0,0 +1,8 @@
+function myName(){
+    const str = "  Aditya Shaw    "
+    console.log("afterTrim- " + str.trim())
+    console.log("converting into lowerCase- " + str.toLowerCase())
+    console.log("converting into upperCase- " + str.toUpperCase())
+}
+
+module.exports.TrimLowerCaseUpperCase = myName
