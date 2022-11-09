cde826ff838e410ca9f3385ce2ad83279193bd59
aadimr <adityashaw9066@gmail.com>
2022-11-09 22:36:07 +0530

    memes creation

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const CowinController= require("../controllers/cowinController")
+const memeController= require("../controllers/memeController")
 
 
 
@@ -9,15 +9,11 @@ router.get("/test-me", function (req, res) {
 })
 
 
-router.get("/cowin/states", CowinController.getStates)
-router.get("/cowin/districtsInState/:stateId", CowinController.getDistricts)
-router.get("/cowin/getByPin", CowinController.getByPin)
-router.get("/cowin/getBydistrict", CowinController.getByDistrict)
 
-router.post("/cowin/getOtp", CowinController.getOtp)
-
-// WRITE A GET API TO GET THE LIST OF ALL THE "vaccination sessions by district id" for any given district id and for any given date
+router.get("/meme", memeController.meme)
+router.post("/Mymeme", memeController.Mymeme)
 
 
 
-module.exports = router;
+
+module.exports = router
